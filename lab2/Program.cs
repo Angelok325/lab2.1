eefb2aeb902dd8b43979b3cde0c8e2714644765b
12 <12@DESKTOP-K6U2P3E>
2023-10-31 12:33:29 +0300

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите f, n");
+double f = Convert.ToDouble(Console.ReadLine());
+double n = Convert.ToDouble(Console.ReadLine());
+if (f != n)
+{
+    if (f > n)
+    {
+        n = f;
+    }
+    else
+    {
+        f = n;
+    }
+}
+else
+{
+    f = 0;
+    n = 0;
+}
+Console.WriteLine($"{f}, {n}");
